7fcce5ef2b93342146b0ffb540f7c1791f09f668
ashish <ash.manmode@gmail.com>
2016-09-07 17:26:40 +0530

    XOR DNN

---
+++ test.py
@@ -0,0 +1,18 @@
+import theano
+import theano.tensor as T
+import theano.tensor.nnet as nnet
+import numpy as np
+
+x = T.dscalar()
+y = T.exp(T.sin(x**2))
+
+print type(y)
+
+f = theano.function([x],y)
+print f(2.3)
+
+fp = T.grad(y,wrt=x)
+fnew = theano.function([x],fp)
+
+print fnew(2.3)
+
