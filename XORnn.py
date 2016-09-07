eea8fc84d4d9a9a74ca5b032bb690faf5ff6aca3
ashmanmode <ash.manmode@gmail.com>
2016-09-07 18:15:32 +0530

    printing computational graph

--- XORnn.py
+++ XORnn.py
@@ -1,7 +1,11 @@
+import graphviz
+
 import theano
 import theano.tensor as T
 import theano.tensor.nnet as nnet
 import numpy as np
+import pydot
+
 
 x = T.dvector()
 y = T.dscalar()
@@ -25,9 +29,11 @@ hid1 = layer(x,theta1)
 out1 = T.sum(layer(hid1, theta2)) #output layer
 fc = (out1 - y)**2 #cost expression (y is the actula output given while training)
 
+
 cost = theano.function(inputs=[x, y], outputs=fc, updates=[
         (theta1, grad_desc(fc, theta1)),
         (theta2, grad_desc(fc, theta2))])
+theano.printing.pydotprint(cost, outfile="logreg_pydotprint_cost_pre.png", var_with_name_simple=True)  
 run_forward = theano.function(inputs=[x], outputs=out1)
 
 #training
@@ -45,3 +51,6 @@ print(run_forward([0,1]))
 print(run_forward([1,1]))
 print(run_forward([1,0]))
 print(run_forward([0,0]))
+
+theano.printing.debugprint(cost);
+theano.printing.pydotprint(cost, outfile="logreg_pydotprint_cost.png", var_with_name_simple=True)  
